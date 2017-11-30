66f13e5c52f899609b1a571966f08b07bc4326dc
h4x9r <h4x9r@bk.ru>
2017-11-30 16:02:15 +0100

    trainiereSem() hinzugefuegt

--- src/sem/systemfunktionen/assistent/impl/IAssistentKonfigurationImpl.java
+++ src/sem/systemfunktionen/assistent/impl/IAssistentKonfigurationImpl.java
@@ -4,18 +4,30 @@ import sem.datenhaltung.semmodel.entities.AssistentKonfig;
 import sem.systemfunktionen.assistent.services.IAssistentKonfiguration;
 
 public class IAssistentKonfigurationImpl implements IAssistentKonfiguration {
+
+
     @Override
-    public boolean setAssiKonfig(AssistentKonfig konfig) {
+    public boolean setAssistentKonfig(AssistentKonfig konfig) {
         return false;
     }
 
     @Override
-    public AssistentKonfig getAssiKonfig() {
+    public AssistentKonfig getAssistentKonfig() {
         return null;
     }
 
     @Override
     public boolean setModel(String pfad) {
+        return false;
+    }
+
+    @Override
+    public boolean saveModel(String pfad) {
+        return false;
+    }
+
+    @Override
+    public boolean loadModel(String pfad) {
         return false;
     }
 }
