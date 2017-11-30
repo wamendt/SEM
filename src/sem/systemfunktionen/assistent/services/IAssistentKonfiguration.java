e11b6f4f03be25e9c3fdc1c4ef6e728018785fa7
h4x9r <h4x9r@bk.ru>
2017-11-30 16:02:41 +0100

    Ueberarbeitung der schnitstelle zum Assistenten

--- src/sem/systemfunktionen/assistent/services/IAssistentKonfiguration.java
+++ src/sem/systemfunktionen/assistent/services/IAssistentKonfiguration.java
@@ -3,7 +3,10 @@ package sem.systemfunktionen.assistent.services;
 import sem.datenhaltung.semmodel.entities.AssistentKonfig;
 
 public interface IAssistentKonfiguration {
-    boolean setAssiKonfig(AssistentKonfig konfig);
-    AssistentKonfig getAssiKonfig();
+    boolean setAssistentKonfig(AssistentKonfig konfig);
+    AssistentKonfig getAssistentKonfig();
     boolean setModel(String pfad);
+    boolean saveModel(String pfad);
+    boolean loadModel(String pfad);
+
 }
