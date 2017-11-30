dd637fdb80851e4718a7f828d8d9c8dc54a2f634
wamendt <w.amendt@live.de>
2017-11-30 01:43:59 +0100

    Methode createMail der DB-Struktur angepasst

--- src/sem/datenhaltung/semmodel/impl/CRUDEMail.java
+++ src/sem/datenhaltung/semmodel/impl/CRUDEMail.java
@@ -13,31 +13,39 @@ public class CRUDEMail extends DBCRUDTeamplate<EMail> implements ICRUDMail{
 
     @Override
     protected EMail makeObject(ResultSet rs) throws SQLException, IOException {
-        int mid = rs.getInt(1);
-        String betreff = rs.getString(2);
-        String inhalt = rs.getString(3);
-        int tid = rs.getInt(4);
-        String absender = rs.getString(5);
         EMail email = new EMail();
-        email.setMid(mid);
-        email.setTid(tid);
-        email.setBetref(betreff);
-        email.setInhalt(inhalt);
-        email.setAbsender(absender);
+        email.setMid(rs.getInt("mid"));
+        email.setBetref(rs.getString("betreff"));
+        email.setInhalt(rs.getString("inhalt"));
+        email.setTid(rs.getInt("tid"));
+        email.setAbsender(rs.getString("absender"));
+        email.setCc(rs.getString("cc"));
+        email.setBcc(rs.getString("bcc"));
+        email.setEmpfaenger(rs.getString("empfaenger"));
+        email.setContentOriginal(rs.getString("contentOriginal"));
+        email.setZustand(rs.getString("zustand"));
+        email.setMessageID(rs.getString("messageID"));
         return email;
     }
 
     @Override
     public int createEMail(EMail email) throws IOException, SQLException {
-        String sql = "INSERT INTO email (betreff ,inhalt , tid, absender)" +
-                "VALUES ( ?, ?, ?, ?)";
+        String sql = "INSERT INTO email (betreff ,inhalt , tid, absender, cc, bcc, empfaenger, contentOriginal, zustand, messageID)" +
+                "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
-        return insertAndReturnKey(sql, email.getBetref());
+        return insertAndReturnKey(sql, email.getBetreff(), email.getInhalt(), email.getTid(), email.getAbsender(),
+                email.getCc(), email.getBcc(), email.getEmpfaenger(), email.getContentOriginal(), email.getZustand(),
+                email.getMessageID());
     }
 
     @Override
     public EMail getEMailById(int mid) throws IOException, SQLException {
-        ArrayList<EMail> eMails = query("SELECT * FROM email WHERE mid=?", mid);
+        ArrayList<EMail> eMails = query("SELECT * FROM email WHERE mid = ?", mid);
+        return eMails.size() > 0 ? eMails.get(0) : null;
+    }
+
+    public EMail getEMailByMessageID(String id) throws IOException, SQLException {
+        ArrayList<EMail> eMails = query("SELECT * FROM email WHERE messageID = ?", id);
         return eMails.size() > 0 ? eMails.get(0) : null;
     }
 
@@ -61,7 +69,7 @@ public class CRUDEMail extends DBCRUDTeamplate<EMail> implements ICRUDMail{
     public boolean updateEMail(EMail email) throws IOException, SQLException {
         int ret = updateOrDelete("UPDATE email" +
                         " SET betreff = ? , inhalt = ?, tid = ?, absender = ? " +
-                        "WHERE mid = ?", email.getBetref(), email.getInhalt()
+                        "WHERE mid = ?", email.getBetreff(), email.getInhalt()
                 ,email.getTid(), email.getAbsender());
         return ret == 1? true : false;
     }
