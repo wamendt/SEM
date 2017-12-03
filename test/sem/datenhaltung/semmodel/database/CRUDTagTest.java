c96179b402aa13aeaea4643122e5a2a54382d2ee
h4x9r <h4x9r@bk.ru>
2017-12-03 15:37:59 +0100

    s

--- test/sem/datenhaltung/semmodel/database/CRUDTagTest.java
+++ test/sem/datenhaltung/semmodel/database/CRUDTagTest.java
@@ -1,7 +1,11 @@
 package sem.datenhaltung.semmodel.database;
 
 import org.junit.jupiter.api.Test;
+import sem.datenhaltung.semmodel.entities.EMail;
 import sem.datenhaltung.semmodel.entities.Tag;
+import sem.datenhaltung.semmodel.services.ICRUDMail;
+import sem.datenhaltung.semmodel.services.ICRUDManagerSingleton;
+import sem.datenhaltung.semmodel.services.ICRUDTag;
 
 import java.io.IOException;
 import java.sql.SQLException;
@@ -11,12 +15,21 @@ class CRUDTagTest {
     void createTag() throws IOException, SQLException {
         Tag tag = new Tag();
         tag.setName("projekt");
+        ICRUDTag crudtag = ICRUDManagerSingleton.getIcrudTagInstance();
+        crudtag.createTag(tag);
+
 
 
     }
 
     @Test
-    void getTagById() {
+    void getTagById() throws IOException, SQLException {
+        ICRUDMail icrudMail = ICRUDManagerSingleton.getIcrudMailInstance();
+        EMail eMail = icrudMail.getEMailById(123);
+        ICRUDTag crudtag = ICRUDManagerSingleton.getIcrudTagInstance();
+        Tag tag = crudtag.getTagById(1);
+        eMail.setTid(tag.getTid());
+        icrudMail.updateEMail(eMail);
     }
 
     @Test
@@ -24,7 +37,9 @@ class CRUDTagTest {
     }
 
     @Test
-    void deleteTag() {
+    void deleteTag() throws IOException, SQLException {
+        ICRUDTag crudtag = ICRUDManagerSingleton.getIcrudTagInstance();
+        crudtag.deleteTag(1);
     }
 
     @Test
