9bcfcb475d7646e99153e995ac643a94d04990f7
h4x9r <h4x9r@bk.ru>
2017-12-03 15:39:47 +0100

    DelteWoerterMitTagId erstellt

--- src/sem/fachlogik/assistentsteuerung/services/IAssistentSteuerung.java
+++ src/sem/fachlogik/assistentsteuerung/services/IAssistentSteuerung.java
@@ -1,4 +1,20 @@
 package sem.fachlogik.assistentsteuerung.services;
 
+import java.io.IOException;
+import java.sql.SQLException;
+
 public interface IAssistentSteuerung {
+
+    /**
+     * Trainiert SEM mit den EMail instanzen aus der DB,
+     * dabei werden Statistiken fuer jede EMail erstellt, sowie die passenden Tags
+     * ausgewaehlt. Diese Methode erstellt ein komplett neues Model.
+     */
+    void trainiereSEM(int numTopics, double alphasum, double beta) throws IOException, SQLException;
+
+
+    /**
+     * Trainiert ein vorhandenes Modell, mit den Instanzen aus der DB.
+     */
+    void trainiereVorhandenSEM() throws IOException, SQLException;
 }
