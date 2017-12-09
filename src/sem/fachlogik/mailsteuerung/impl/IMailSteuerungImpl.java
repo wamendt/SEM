fe7c96b445eb58e327b1212dc21a3c4c55383312
wamendt <w.amendt@live.de>
2017-12-09 21:28:02 +0100

    Mailsteuerung angepasst

--- src/sem/fachlogik/mailsteuerung/impl/IMailSteuerungImpl.java
+++ src/sem/fachlogik/mailsteuerung/impl/IMailSteuerungImpl.java
@@ -1,6 +1,30 @@
 package sem.fachlogik.mailsteuerung.impl;
 
+import sem.fachlogik.mailsteuerung.listener.MsgReceivedListener;
+import sem.fachlogik.mailsteuerung.listener.MsgRemovedListener;
 import sem.fachlogik.mailsteuerung.services.IMailSteuerung;
+import sem.fachlogik.mailsteuerung.utils.impl.IMailServiceImpl;
+import sem.fachlogik.mailsteuerung.utils.services.IMailService;
 
 public class IMailSteuerungImpl implements IMailSteuerung{
+
+    @Override
+    public void addMsgReceivedListener(MsgReceivedListener msgReceivedListener) {
+        IMailServiceImpl.getReceivedListeners().add(msgReceivedListener);
+    }
+
+    @Override
+    public void removeMsgReceivedListener(MsgReceivedListener msgReceivedListener) {
+        IMailServiceImpl.getReceivedListeners().remove(msgReceivedListener);
+    }
+
+    @Override
+    public void addMsgRemovedListener(MsgRemovedListener msgRemovedListener) {
+        IMailServiceImpl.getRemovedListeners().add(msgRemovedListener);
+    }
+
+    @Override
+    public void removeMsgRemovedListener(MsgRemovedListener msgRemovedListener) {
+        IMailServiceImpl.getRemovedListeners().remove(msgRemovedListener);
+    }
 }
