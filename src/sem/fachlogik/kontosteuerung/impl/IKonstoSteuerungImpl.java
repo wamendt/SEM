5feba79e0aab06d38ce83717e0da4b1c35f89ec6
h4x9r <h4x9r@bk.ru>
2017-12-12 20:16:29 +0100

    Konto Dummy implementiert zum getKonto

--- src/sem/fachlogik/kontosteuerung/impl/IKonstoSteuerungImpl.java
+++ src/sem/fachlogik/kontosteuerung/impl/IKonstoSteuerungImpl.java
@@ -1,4 +1,17 @@
 package sem.fachlogik.kontosteuerung.impl;
 
-public class IKonstoSteuerungImpl {
+import sem.fachlogik.grenzklassen.KontoGrenz;
+import sem.fachlogik.kontosteuerung.services.IKontoSteuerung;
+
+public class IKonstoSteuerungImpl implements IKontoSteuerung{
+
+    @Override
+    public KontoGrenz getKonto(int id) {
+        KontoGrenz konto = new KontoGrenz();
+        konto.setEmailAddress("w.amendt@gmx.de");
+        konto.setIMAPhost("imap.gmx.net");
+        konto.setSMTPhost("smtp.gmx.net");
+        konto.setPassWort("Packard1");
+        return konto;
+    }
 }
--- src/sem/fachlogik/kontosteuerung/services/IKontoSteuerung.java
+++ src/sem/fachlogik/kontosteuerung/services/IKontoSteuerung.java
@@ -1,4 +1,12 @@
 package sem.fachlogik.kontosteuerung.services;
 
+import sem.fachlogik.grenzklassen.KontoGrenz;
+
 public interface IKontoSteuerung {
+    /**
+     * holt ein Kontogrenzobjekt aus der datenbank
+     * @param id
+     * @return
+     */
+    KontoGrenz getKonto(int id);
 }
