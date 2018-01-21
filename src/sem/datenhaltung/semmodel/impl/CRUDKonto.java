2703558497716efe994eda462faf20d2431a7144
maksim.hvatov <h4x9r@bk.ru>
2018-01-21 20:10:49 +0100

    Neue Aenderungen in der mailsteuerung

--- src/sem/datenhaltung/semmodel/impl/CRUDKonto.java
+++ src/sem/datenhaltung/semmodel/impl/CRUDKonto.java
@@ -2,10 +2,24 @@ package sem.datenhaltung.semmodel.impl;
 
 import sem.datenhaltung.semmodel.entities.Konto;
 import sem.datenhaltung.semmodel.services.ICRUDKonto;
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * CRUD Klasse die die ICRUDKonto Schnitstelle implementiert.
  * Bietet Methoden fuer den Zugriff auf die Kontos aus der Datenbank.
@@ -40,6 +54,7 @@ public class CRUDKonto extends DBCRUDTeamplate<Konto> implements ICRUDKonto {
 
     @Override
     public int createKonto(Konto konto){
+        konto.setPassWort(encryptPassword(konto.getPassWort()));
         try {
             int id = insertAndReturnKey(SQL_INSERT_KONTO, konto.getUserName(),
                     konto.getIMAPhost(), konto.getPassWort(), konto.getPort(), konto.getSMTPhost(), konto.getSignatur());
@@ -81,7 +96,10 @@ public class CRUDKonto extends DBCRUDTeamplate<Konto> implements ICRUDKonto {
         ArrayList<Konto> kontos;
         try {
             kontos = query(String.format(SQL_SELECT_FROM_WHERE, TABLE_NAME, COLUMN_KID), kid);
-            return kontos.size() > 0 ? kontos.get(0) : null;
+            if(kontos.size() > 0){
+                kontos.get(0).setPassWort(decryptPassword(kontos.get(0).getPassWort()));
+                return kontos.get(0);
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -91,11 +109,101 @@ public class CRUDKonto extends DBCRUDTeamplate<Konto> implements ICRUDKonto {
     @Override
     public ArrayList<Konto> getAlleKonten(){
         try {
-            return query(String.format(SQL_SELECT_FROM, TABLE_NAME));
+            ArrayList<Konto> kontos =  query(String.format(SQL_SELECT_FROM, TABLE_NAME));
+            for(Konto k : kontos){
+                k.setPassWort(decryptPassword(k.getPassWort()));
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
         return new ArrayList();
     }
 
+    /**
+     * Methode zum verschluesseln eines PlainText passwortes
+     * @param userPWD
+     * @return
+     */
+    private String encryptPassword(String userPWD){
+        String encryptedPassword = "";
+        try {
+            //Key generieren
+            SecretKeySpec secretKeySpec = generateCrypKey();
+
+            // Verschluesseln
+            Cipher cipher = Cipher.getInstance("AES");
+            cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec);
+            byte[] encrypted = cipher.doFinal(userPWD.getBytes());
+
+            // bytes zu Base64-String konvertieren (dient der Lesbarkeit)
+            BASE64Encoder myEncoder = new BASE64Encoder();
+            encryptedPassword = myEncoder.encode(encrypted);
+        }
+        catch (NoSuchAlgorithmException | BadPaddingException | NoSuchPaddingException | IllegalBlockSizeException
+                | InvalidKeyException e) {
+            System.out.println("Beim Verschlüssen wurde eine Exception geworfen: " + e.getMessage() + "\n");
+            e.printStackTrace();
+
+        }
+        return encryptedPassword;
+    }
+
+
+    /**
+     * Methode zum entschliesseln eines Plaintextpasswortes
+     * @param userPWD
+     * @return
+     */
+    private String decryptPassword(String userPWD){
+        String decryptedPassword = "";
+        try {
+            //Key generieren
+            SecretKeySpec secretKeySpec = generateCrypKey();
+
+            // BASE64 String zu Byte-Array konvertieren
+            BASE64Decoder myDecoder2 = new BASE64Decoder();
+            byte[] crypted2 = myDecoder2.decodeBuffer(userPWD);
+
+            // Entschluesseln
+            Cipher cipher2 = null;
+            cipher2 = Cipher.getInstance("AES");
+            cipher2.init(Cipher.DECRYPT_MODE, secretKeySpec);
+            byte[] cipherData2 = new byte[0];
+            cipherData2 = cipher2.doFinal(crypted2);
+            decryptedPassword = new String(cipherData2);
+
+        }
+        catch (NoSuchAlgorithmException | InvalidKeyException | BadPaddingException
+                | IllegalBlockSizeException | NoSuchPaddingException | IOException e) {
+            System.out.println("Beim Entschlüssen wurde eine Exception geworfen: " + e.getMessage() + "\n");
+            e.printStackTrace();
+        }
+        return decryptedPassword;
+    }
+
+
+    /**
+     * Hilfstmethode zum ent und verschluesseln von passwoertern
+     * @return
+     */
+    private SecretKeySpec generateCrypKey(){
+        //Key generieren, Den Wert letzendlich aus der DB holen
+        String cryptKey = "123456789";
+        SecretKeySpec secretKeySpec = null;
+        byte[] key = new byte[0];
+        try {
+            key = cryptKey.getBytes("UTF-8");
+
+            MessageDigest sha = null;
+            sha = MessageDigest.getInstance("SHA-256");
+            key = sha.digest(key);
+            key = Arrays.copyOf(key, 16);
+
+            secretKeySpec = new SecretKeySpec(key, "AES");
+        } catch (UnsupportedEncodingException | NoSuchAlgorithmException e) {
+            System.out.println("Beim Generieren des Schlüssels wurde eine Exception geworfen: " + e.getMessage() + "\n");
+            e.printStackTrace();
+        }
+        return secretKeySpec;
+    }
 }
